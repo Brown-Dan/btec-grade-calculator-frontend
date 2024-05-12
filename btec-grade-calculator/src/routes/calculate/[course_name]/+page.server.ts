dc0db0925f1a891b876cc2c89d85142cad206ddb
daniel.brown <daniel.brown@autotrader.co.uk>
2024-05-13 00:38:11 +0100

    changed to prod url

--- btec-grade-calculator/src/routes/calculate/[course_name]/+page.server.ts
+++ btec-grade-calculator/src/routes/calculate/[course_name]/+page.server.ts
@@ -51,7 +51,7 @@ export const actions = {
             }));
 
             const requestBody: string = JSON.stringify({courseType: course_type, units: units})
-            const response: Response = await fetch('http://localhost:8080/calculate', {
+            const response: Response = await fetch('https://btec-grade-calculator-4a817c201846.herokuapp.com/calculate', {
                 method: 'POST',
                 body: requestBody,
                 headers: {
